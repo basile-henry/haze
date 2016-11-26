feaeae11aa09481b1da082225faf9a7181304760
Basile Henry <basile.henry@hotmail.com>
2016-11-26 15:13:01 +0000

    Latest build

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf dist
-mkdir dist
-cp src/index.html dist/
-cp res/favicon.png dist/
-elm make src/Main.elm --yes --output dist/temp.js
-uglifyjs dist/temp.js --compress --output dist/main.js &> /dev/null
-rm dist/temp.js
-echo "Done!"
